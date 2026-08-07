a76c6cbc1b9d7ca1a1f265bff95c9b03a6bea72e
Adama Coulibaly <101523227+manimaneya@users.noreply.github.com>
2026-08-07 18:52:07 +0000

    Add payment function for Ikagne Mobile Money

---
+++ js/js/payment.js
@@ -0,0 +1,13 @@
+// Paiement Ikagne Mobile Money
+
+function payerProduit() {
+
+    alert(
+        "Choisissez votre moyen de paiement :\n\n" +
+        "Wave\n" +
+        "Orange Money\n" +
+        "MTN Money\n" +
+        "Moov Money"
+    );
+
+}
